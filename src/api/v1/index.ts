a115048997dbacb1b1454c25c58b054a7e1af7b3
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-11-19 16:12:17 +0900

    [ADD] PostController Repo

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+
 import auth from './auth';
+import post from './post';
+
 const router = Router();
 
 router.use('/auth', auth);
+router.use('/post', post);
 
 export default router;
